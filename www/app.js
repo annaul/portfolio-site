556a731b883f1ad5ec1c770f9aac39f17615310e
Anna Ulanova <anetteua0@gmail.com>
2017-09-12 16:01:47 -0700

    s

--- www/app.js
+++ www/app.js
@@ -1,4 +1,4 @@
 'use strict';
 
-$('.arrow').hide().delay(5000).show(0);
-$('.arrow').delay(1000).animate({ marginTop: '25vh' });
+$('.arrow').hide().delay(5000).fadeIn('slow');
+$('.arrow').delay(1000).animate({ marginTop: '25vh' }, 1000);
