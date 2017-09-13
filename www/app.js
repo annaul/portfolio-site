c210d2ceb177c7de41d7a3cb0d11ca91a842b014
Anna Ulanova <anetteua0@gmail.com>
2017-09-12 23:07:49 -0700

    s

--- www/app.js
+++ www/app.js
@@ -1,7 +1,28 @@
 'use strict';
-
+/*arrow animation*/
 $('.arrow').hide().delay(5000).show(0);
-$('.arrow').animate({ marginTop: '23vh' }, 3000);
+$('.arrow').animate({ marginTop: '23vh' }, 1000);
 
-// .delay(1000)
-// .fadeIn(2000)
+/*projets title*/
+$('.new-title').load(location.href + " #demo")
+var title = $('#projects-title').text();
+var $num = title.length + 4; //num of columns
+var property = 'repeat(' + $num + ', 1fr)'
+var titleArr = title.toUpperCase().split('');
+
+$('.new-title').css({
+  'grid-template-columns': property,
+  'grid-template-rows': 'repeat(1, 1fr)'
+});
+function insertChar(titleArr) {
+  for (var i = 0; i < titleArr.length; i ++) {
+    var colNum = i + 3;
+    $('<span>' + titleArr[i] + '</span>').appendTo('.new-title')
+    .css({
+      'grid-column': String(colNum),
+      'grid-row': '1',
+      'text-align': 'center'
+    })
+  };
+}
+insertChar(titleArr);
